49488eeb6f677311708290ee3534e48e7b1560dc
md4648 <124080342+md4648@users.noreply.github.com>
2023-12-31 00:16:31 +0300

    complete sign up page and sign in api text

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Header  from "./componets/Header";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+    
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+
+    
+
+  
+  
+  );
+}
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import mongoose from "mongoose"
+mongoose.connect()
+
+export default function SignIn() {
+  return (
+    <div>
+      Sign In
+    </div>
+  )
+}
